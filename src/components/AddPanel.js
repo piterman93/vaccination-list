45f2014581ac49cf3b708b8d9f625c41895a03d2
Piotr Maniak <piotrmaniak93@gmail.com>
2021-05-24 18:50:40 +0200

    first commit application core

---
+++ src/components/AddPanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddPanel = () => {
+  return (
+    <div className="panel">
+      <h1>Aplikacja: lista szczepień w dniu dd/mm/yyyy</h1>
+      <p>Lista zaszczepionych</p>
+    </div>
+  );
+};
+
+export default AddPanel;
---
+++ src/components/Item.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Item = () => {
+  return <li>Osoba Zaszczepiona</li>;
+};
+
+export default Item;
