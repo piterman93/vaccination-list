0ff122c6f2fad476b0b5ec88d1f77d942d43fa1e
Piotr Maniak <piotrmaniak93@gmail.com>
2021-05-26 19:19:30 +0200

    AddPanel add

--- src/components/AddPanel.js
+++ src/components/AddPanel.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./AddPanel.css";
 
 const AddPanel = () => {
+  const currentTime = new Date().getTime();
+  const date = new Date(currentTime).toLocaleDateString();
+
+  const [name, setName] = useState("");
+  const [vaccined, setVaccined] = useState(false);
+  const [firstDate, setFirstDate] = useState("");
+
   return (
     <div className="panel">
-      <h1>Aplikacja: lista szczepień w dniu dd/mm/yyyy</h1>
-      <p>Dodaj osobę</p>
+      <h1>Aplikacja: lista szczepień w dniu {date}</h1>
+      <input
+        type="text"
+        placeholder="Imię i nazwisko"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="user">
+        {" "}
+        <input
+          type="checkbox"
+          id="user"
+          checked={vaccined}
+          onChange={(e) => setVaccined(e.target.checked)}
+        />
+        Pierwsza dawka
+      </label>
+      <br />
+      <label>
+        Data pierwszego szczepienia{" "}
+        <input
+          type="date"
+          value={firstDate}
+          onChange={(e) => setFirstDate(e.target.value)}
+        />
+      </label>
+      <br />
+      <button>Zapisz pacjenta</button>
     </div>
   );
 };
